85b36ca49a4601951d1d5b7bbc21d88b6b96d5dc
abdul-wahab619 <awminhas619@gmail.com>
2025-08-24 23:48:34 +0500

    Portfolio: update portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,9 @@
-import travel from "../assets/images/TRAVEL.png";
-import movies1 from "../assets/images/MOVIES_.png";
+import aistartup from "../assets/images/3d-startup-app.png";
+import instella from "../assets/images/Instella.png";
 import movies2 from "../assets/images/MOVIES2_.png";
-import portfolio1 from "../assets/images/PORTFOLIO-.png";
+import movieApp from "../assets/images/Movie-app.png";
 import portfolio2 from "../assets/images/PORTFOLIO..png";
-import carRental from "../assets/images/CAR_RENTAL_.png";
+import shoeVista from "../assets/images/shoeVista.png";
 
 const Portfolio = () => {
   return (
@@ -17,15 +17,18 @@ const Portfolio = () => {
       <div className="portfolios">
         <div className="portfolio-item">
           <div className="image">
-            <img src={travel} alt="travel" />
+            <img src={aistartup} alt="aistartup" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>3D Startup App</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/abdul-wahab619/3D-Startup-App"
+                className="icon"
+              >
                 <i className="fab fa-github"></i>
               </a>
-              <a href="#" className="icon">
+              <a href="https://aistartupapp.netlify.app/" className="icon">
                 <i className="fas fa-link"></i>
               </a>
             </div>
@@ -34,15 +37,21 @@ const Portfolio = () => {
 
         <div className="portfolio-item">
           <div className="image">
-            <img src={movies1} alt="" />
+            <img src={instella} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>Instella Mobile App</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/abdul-wahab619/Instella-mobileApp"
+                className="icon"
+              >
                 <i className="fab fa-github"></i>
               </a>
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/abdul-wahab619/Instella-mobileApp"
+                className="icon"
+              >
                 <i className="fas fa-link"></i>
               </a>
             </div>
@@ -51,15 +60,18 @@ const Portfolio = () => {
 
         <div className="portfolio-item">
           <div className="image">
-            <img src={portfolio1} alt="" />
+            <img src={movieApp} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>Movie App</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/abdul-wahab619/movie-app-appwrite"
+                className="icon"
+              >
                 <i className="fab fa-github"></i>
               </a>
-              <a href="#" className="icon">
+              <a href="https://movie-app-appwrite.vercel.app/" className="icon">
                 <i className="fas fa-link"></i>
               </a>
             </div>
@@ -68,15 +80,18 @@ const Portfolio = () => {
 
         <div className="portfolio-item">
           <div className="image">
-            <img src={carRental} alt="" />
+            <img src={shoeVista} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>Shoe Vista</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/abdul-wahab619/ShoeVista"
+                className="icon"
+              >
                 <i className="fab fa-github"></i>
               </a>
-              <a href="#" className="icon">
+              <a href="https://shoe-vista.vercel.app/" className="icon">
                 <i className="fas fa-link"></i>
               </a>
             </div>
