775163d951362428628d1e5ceb476c32371ddb33
abdul-wahab619 <awminhas619@gmail.com>
2025-08-30 22:34:03 +0500

    Portfolio: update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,8 @@ import aistartup from "../assets/images/3d-startup-app.png";
 import instella from "../assets/images/Instella.png";
 import aiResumeBuilder from "../assets/images/AI-Resume-Builder.png";
 import movieApp from "../assets/images/Movie-app.png";
+import learnPath from "../assets/images/learn-path.png";
+
 import aiMedlab from "../assets/images/AI-MedLab.png";
 import shoeVista from "../assets/images/shoeVista.png";
 
@@ -15,6 +17,26 @@ const Portfolio = () => {
       </div>
       <p className="port-text">Here Is Some Of My Professional Work.</p>
       <div className="portfolios">
+        <div className="portfolio-item">
+          <div className="image">
+            <img src={learnPath} alt="AI-Powered Learning Path" />
+          </div>
+          <div className="hover-items">
+            <h3>AI-Learning Path </h3>
+            <div className="icons">
+              <a
+                href="https://github.com/abdul-wahab619/AI-Powered-Personalized-Learning-Roadmap"
+                className="icon"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://learnpath-ai.netlify.app/" className="icon">
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="portfolio-item">
           <div className="image">
             <img src={aistartup} alt="aistartup" />
